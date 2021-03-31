45d80177e694b407ce3879daf1f2ec91bdb2eaab
Erkedo Cara <erkedo.cara@sisal.al>
2021-03-31 15:30:59 +0200

    worked version arduino

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Z
+*src/main/java/arduino/BasicLEDExample.java,d\d\dd250ae9bc99a730c0b3160d4857fb8645cfc450
---
+++ src/main/java/arduino/BasicLEDExample.java
@@ -0,0 +1,28 @@
+package arduino;
+
+import java.util.Scanner;
+
+
+public class BasicLEDExample {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner ob = new Scanner(System.in);
+        String ArduinoPort = "COM3"; //Your port name here
+        int BAUD_RATE = 9600;
+        Arduino arduino = new Arduino(ArduinoPort, BAUD_RATE);
+
+        arduino.openConnection();
+        arduino.serialWrite('1'); //serialWrite is an overridden method, allowing both characters and strings.//its second parameter even allows delays. more details can be found in the documentation.
+        char input = ob.nextLine().charAt(0);
+        while(input != 'n'){
+            arduino.serialWrite(input,20);
+            input = ob.nextLine().charAt(0);
+        }
+        ob.close();
+        arduino.closeConnection();
+
+
+    }
+
+}
